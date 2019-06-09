096eca5a1fafb401134a6c488ab340cdb3cd3dde
Snake8859 <654285370@qq.com>
2019-06-09 17:46:37 +0800

    启动类添加注释

--- vueDemo/src/main/java/com/snake/vue/VueApp.java
+++ vueDemo/src/main/java/com/snake/vue/VueApp.java
@@ -3,6 +3,9 @@ package com.snake.vue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Springboot启动类
+ */
 @SpringBootApplication
 public class VueApp {
 
